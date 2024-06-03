fa6a0b07cf003e88462a5321854e9221174de0d3
Bandasack Ethan <ethan.bandasack@student-cs.fr>
2024-06-03 10:01:38 +0200

    test

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -7,8 +7,11 @@ function Home() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.jsx</code> and save to reload.
+          Bonjour
         </p>
+        <div>
+          <input type="Search" />
+        </div>
         <a
           className="App-link"
           href="https://react.dev"
